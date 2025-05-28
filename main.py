9f4425affb89e9c0e82900d9fea6ffe9ed68ddfd
ekremkripto <ekremkripto633@gmail.com>
2025-05-28 12:24:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def load_coins():
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
+        "chat_id": CHAT_ID,
         "text": message
     }
     try:
